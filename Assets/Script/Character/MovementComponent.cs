29d064ca87ab3517f8b9a2181513080766db4c65
Talha991s <55731371+Talha991s@users.noreply.github.com>
2021-01-25 20:01:45 -0800

    My player is not moving

--- Assets/Script/Character/MovementComponent.cs
+++ Assets/Script/Character/MovementComponent.cs
@@ -1,16 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Character
 {
+   
     public class MovementComponent : MonoBehaviour
     {
+        [SerializeField] private float WalkSpeed;
+        [SerializeField] private float RunSpeed;
+        [SerializeField] private float JumpForce;
+
+        //component
+        private PlayerController PlayerController;
+        private Vector2 InputVector = Vector2.zero;
+
+       //   private GameInputActions inputActions;
+
+        private void Awake()
+        {
+            PlayerController = GetComponent<PlayerController>();
+        }
+
+        private void Start()
+        {
+            Debug.Log("Start");
+            
+        }
+        private void Update()
+        {
+            if (!(InputVector.magnitude > 0)) return;
+            if (PlayerController.isJumping) return;
+
+            Vector3 moveDirection = transform.forward * InputVector.y + transform.right * InputVector.x;
+            
+            float currentSpeed = PlayerController.isRunning ? RunSpeed : WalkSpeed;
+
+            Vector3 movementDirection = moveDirection * (currentSpeed * Time.deltaTime);
+
+            transform.position += movementDirection;
+        }
+
         public void OnMovement(InputValue value)
         {
-            Debug.Log(value.Get<Vector2>());
-        }    
+            InputVector = value.Get<Vector2>();
+            //Debug.Log(value.Get());
+        }
+
     }
 
 }
